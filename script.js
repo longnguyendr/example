dbc6a884504170d9f3063e28b27c84bb00105ecd
longnguyendr <long.nguyendr@gmail.com>
2018-09-24 21:20:24 +0300

    initial

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function jsontostring() {
+    
+    var jsonobj=JSON.parse(userData);
+    var form = document.getElementById("test");
+    var contents = form.querySelectorAll("input, select, textarea");
+    
+    for (var i = 0; i < contents.length; ++i) {
+        var content = contents[i];
+        var names = content.name;
+        var values = content.value;
+        var checks = content.checked;
+        var types = content.type;
+        
+        for (var x in jsonobj) {
+            if (names == x) {   
+                // Set Radio type
+                if (types=="radio" && values == jsonobj[x]) {
+                    checks = true;
+                    content.checked = checks;
+                } 
+                // Set Checkbox type
+                else if (types=="checkbox") {
+                    for (z in jsonobj[x]) {
+                        if (values == jsonobj[x][z]) {
+                            checks=true;
+                            content.checked = checks;
+                        }
+                    }
+                }
+                // Set value for input
+                else {
+                    values = jsonobj[x];
+                    content.value = jsonobj[x];
+                }
+            }
+        }
+    }
+})
